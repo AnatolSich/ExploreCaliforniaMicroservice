bc78474f9e1af4115e4f8e5d69e50231e8eb42b1
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-05-20 14:43:46 +0300

    Added pages to repo method

--- src/main/java/com/example/explore/exploreCalifornia/repo/TourRepository.java
+++ src/main/java/com/example/explore/exploreCalifornia/repo/TourRepository.java
@@ -1,11 +1,20 @@
 package com.example.explore.exploreCalifornia.repo;
 
 import com.example.explore.exploreCalifornia.domain.Tour;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface TourRepository extends CrudRepository<Tour, Integer> {
-    List<Tour> findByTourPackageCode(@Param("code")String code);
+public interface TourRepository extends PagingAndSortingRepository<Tour, Integer> {
+    /**
+     * Find Tours associated with the Tour Package.
+     *
+     * @param code tour package code
+     * @return List of found tours.
+     */
+    Page<Tour> findByTourPackageCode(@Param("code")String code, Pageable pageable);
 }
