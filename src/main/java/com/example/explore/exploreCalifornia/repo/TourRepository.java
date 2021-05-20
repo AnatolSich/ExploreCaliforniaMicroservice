674079f8331062649fe72329bd8d6541b506bd03
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-05-20 14:04:18 +0300

    first commit

---
+++ src/main/java/com/example/explore/exploreCalifornia/repo/TourRepository.java
@@ -0,0 +1,7 @@
+package com.example.explore.exploreCalifornia.repo;
+
+import com.example.explore.exploreCalifornia.domain.Tour;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourRepository extends CrudRepository<Tour, Integer> {
+}
