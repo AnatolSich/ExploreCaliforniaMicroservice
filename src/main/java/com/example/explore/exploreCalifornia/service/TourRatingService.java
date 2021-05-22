8d2c0ce3aad71b571e1ab0a678329374534165a0
Anatolii Sichkar <Anatolii_Sichkar@epam.com>
2021-05-22 20:21:32 +0300

    Added transactions to service

--- src/main/java/com/example/explore/exploreCalifornia/service/TourRatingService.java
+++ src/main/java/com/example/explore/exploreCalifornia/service/TourRatingService.java
@@ -9,11 +9,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.OptionalDouble;
 
 @Service
+@Transactional
 public class TourRatingService {
     private TourRatingRepository tourRatingRepository;
     private TourRepository tourRepository;
